414f470bae9ab3bf220d8d8ce5c477b3491598d8
Christina Flores <cdflori1@gmail.com>
2020-11-09 11:35:44 -0700

    Added in html for index and main handlebars files. Adjusted seed.sql in schema.

---
+++ public/assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".devour-form").on("submit", function (event) {
+        event.preventDefault();
+
+        let burger_id = $(this).children(".burger_id").val();
+        console.log(burger_id);
+        $.ajax({
+            mehtod: "PUT",
+            url: "/burgers/" + burger_id
+        }).then(function (data) {
+            location.reload();
+        });
+    });
+});
